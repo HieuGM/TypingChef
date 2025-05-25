c3276d4975059983ef18ed50c5935aa500c8d37b
HieuGM <duchieutuyen@gmail.com>
2025-05-25 22:23:27 +0700

    test

--- core/src/main/java/com/typingchef/models/World.java
+++
@@ -1,4 +0,0 @@
-package com.typingchef.models;
-
-public class World {
-}
--- core/src/main/java/com/typingchef/models/entities/ActionType.java
+++
@@ -1,8 +0,0 @@
-package com.typingchef.models.entities;
-
-public enum ActionType {
-    PREPARE_BREAD,    // Chuẩn bị bánh mì
-    PREPARE_COFFEE,   // Chuẩn bị cà phê
-    SERVE_CUSTOMER,   // Phục vụ khách hàng
-    NONE              // Không có hành động
-}
